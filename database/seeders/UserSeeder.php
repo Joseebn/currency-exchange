32acd6c7ba4f8cba7f2c9f77aa925d32e54cd724
Joseebn <joseebn1988@gmail.com>
2020-10-11 21:16:04 +0200

    first commit api exchange currency

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+        	'username' => 'user_test', 
+        	'password' => Hash::make('pass_test'), 
+        	'api_token' => Str::random(60)
+        ]);
+    }
+}
